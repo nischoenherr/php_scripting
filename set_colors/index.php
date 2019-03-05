0195a070ccd772645052731a9acc228bb9e5cc66
Nick Schönherr <schoenherr@ewerk.de>
2019-03-05 11:54:40 +0100

    Added, updated and fixed colors

---
+++ set_colors/index.php
@@ -0,0 +1,78 @@
+<?php
+
+class Colors // base class
+{
+    private $foreground_colors = array();
+
+    private $background_colors = array();
+
+    public function __construct()
+    {
+        // Set up shell colors
+        $this->foreground_colors['black'] = '0;30';
+        $this->foreground_colors['dark_gray'] = '1;30';
+        $this->foreground_colors['blue'] = '0;34';
+        $this->foreground_colors['green'] = '0;32';
+        $this->foreground_colors['light_green'] = '1;32';
+        $this->foreground_colors['cyan'] = '0;36';
+        $this->foreground_colors['red'] = '0;31';
+        $this->foreground_colors['light_red'] = '1;31';
+        $this->foreground_colors['purple'] = '0;35';
+        $this->foreground_colors['yellow'] = '1;33';
+
+        $this->background_colors['black'] = '40';
+        $this->background_colors['red'] = '41';
+        $this->background_colors['green'] = '42';
+        $this->background_colors['yellow'] = '43';
+        $this->background_colors['blue'] = '44';
+        $this->background_colors['cyan'] = '46';
+        $this->background_colors['light_gray'] = '47';
+    }
+
+    // Returns colored string
+    public function getColoredString($string, $foreground_color = null, $background_color = null)
+    {
+        $colored_string = "";
+
+        // Check if given foreground color found
+        if (isset($this->foreground_colors[$foreground_color])) {
+            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+        }
+        // Check if given background color found
+        if (isset($this->background_colors[$background_color])) {
+            $colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+        }
+
+        // Add string and end coloring
+        $colored_string .= $string . "\033[0m";
+
+        return $colored_string;
+    }
+
+    // Returns all foreground color names
+    public function getForegroundColors()
+    {
+        return array_keys($this->foreground_colors);
+    }
+
+    // Returns all background color names
+    public function getBackgroundColors()
+    {
+        return array_keys($this->background_colors);
+    }
+}
+
+?>
+
+<?php
+
+// Create new Colors class
+$colors = new Colors();
+
+// print some strings with colors
+echo $colors->getColoredString("Praise The Sun.", "yellow", "red") . PHP_EOL;
+echo $colors->getColoredString("Eines Tages, hab ich meinen Maserati.", "black", "cyan") . PHP_EOL;
+echo $colors->getColoredString("Ich sitze tief in einem Ferrari.", "red", "light_gray") . PHP_EOL;
+
+
+?>
---
+++ vowels/index.php
@@ -0,0 +1,82 @@
+<?php
+
+function isVowel($characters) // function to define vowels
+{
+    $characters = strtoupper($characters);
+    return ($characters == 'A' || $characters == 'E' ||
+        $characters == 'I' || $characters == 'O' ||
+        $characters == 'U');
+}
+
+function countVowels($str) // function to count the vowels
+{
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (isVowel($str[$i])) {
+            ++$count;
+        }
+    }
+    return $count;
+}
+
+class Color // base class
+{
+    private $foreground_colors = array();
+
+    private $background_colors = array();
+
+    public function __construct()
+    {
+        // Set up shell colors
+        $this->foreground_colors['green'] = '0;32';
+        $this->foreground_colors['light_green'] = '1;32';
+        $this->foreground_colors['red'] = '0;31';
+        $this->foreground_colors['light_red'] = '1;31';
+    }
+
+    // Returns colored string
+    public function getColoredString($string, $foreground_color = null)
+    {
+        $colored_string = "";
+
+        // Check if given foreground color found
+        if (isset($this->foreground_colors[$foreground_color])) {
+            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+        }
+
+        // Add string and end coloring
+        $colored_string .= $string . "\033[0m";
+
+        return $colored_string;
+    }
+
+    // Returns all foreground color names
+    public function getForegroundColors()
+    {
+        return array_keys($this->foreground_colors);
+    }
+}
+
+// Create new Colors class
+$colors = new Colors();
+
+
+// if isset is used to check the script after values
+if (isset($argv[1])) {
+    $string = $argv[1];
+}
+
+// Error check
+if (empty($string)) {
+    echo 'You need to enter a String!' . PHP_EOL;
+    exit();
+}
+
+// If the User will enter a number
+if (is_numeric($string)) {
+    echo 'Numbers are not allowed!'. PHP_EOL;
+    exit();
+}
+
+// total numbers of vowel
+echo countVowels($string) . PHP_EOL;
